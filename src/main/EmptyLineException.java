76bec2a771f73ef92b72b3bce61cc20b4bd0b28e
spacex33 <3350serge@gmail.com>
2018-01-26 15:22:54 +0200

    examples of using exceptions

---
+++ src/main/EmptyLineException.java
@@ -0,0 +1,9 @@
+public class EmptyLineException extends RuntimeException {
+
+    public EmptyLineException() {
+    }
+
+    public EmptyLineException(String message) {
+        super(message);
+    }
+}
