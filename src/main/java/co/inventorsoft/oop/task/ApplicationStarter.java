697aae841b03c7daf1c8850ea6e8d0fd49ab6cf9
kostyaskrun <35305954+kostyaskrun@users.noreply.github.com>
2018-02-04 13:19:57 +0200

    Add nested try-catch example

--- src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
+++ src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
@@ -7,7 +7,9 @@ import co.inventorsoft.oop.task.shapes.Rectangle;
 import co.inventorsoft.oop.task.shapes.Square;
 import co.inventorsoft.oop.task.shapes.Triangle;
 
+import java.io.*;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class ApplicationStarter {
@@ -21,6 +23,28 @@ public class ApplicationStarter {
         );
         final ComplexShape complexShape = new ComplexShape(shapes);
         System.out.printf("Complex shape area = %f", complexShape.area());
-    }
 
+        //nested try-catch example
+        try {
+            int[] array1 = {1,2,3};
+            System.out.println(array1[10]);
+            try {
+                File file = new File("some.txt");
+                FileReader fr = new FileReader(file);
+                try {
+                    int[] array2 = {1,4,5};
+                    System.out.println(array1[10]);
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("third try block");
+                }
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("second try block");
+            }
+        }
+        catch (RuntimeException e) {
+            System.out.println("first try block");
+        }
+    }
 }
